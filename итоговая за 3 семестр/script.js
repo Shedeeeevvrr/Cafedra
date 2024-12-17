91c86188f2e5f8e01e9e6f41665dac117b13e2fd
Влад <145023961+Shedeeeevvrr@users.noreply.github.com>
2024-12-18 01:39:10 +0500

    Delete итоговая за 3 семестр directory

--- итоговая за 3 семестр/script.js
+++
@@ -1,6 +0,0 @@
-const burgerMenu = document.getElementById('burger-menu');
-const menu = document.getElementById('menu');
-
-burgerMenu.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
